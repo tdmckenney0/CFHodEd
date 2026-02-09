6a0393dc926ecd0b39434d2c49204c1154b586f8
Tanner Mckenney <tmckenney7@outlook.com>
2026-02-09 17:50:05 -0500

    Remove original code

--- D3DHelper/Forms/DeviceTest.vb
+++
@@ -1,5 +0,0 @@
-﻿''' <summary>
-''' Dummy form which is used for temporary device creations.
-''' </summary>
-Friend NotInheritable Class DeviceTest
-End Class
--- HW2HOD/HOD/HODConstants.vb
+++
@@ -1,6 +0,0 @@
-﻿Partial Class HOD
-    Public Const Name_MultiMesh As String = "Homeworld2 Multi Mesh File"
-    Public Const Name_VariableMesh As String = "Homeworld2 Variable Mesh File"
-    Public Const Name_SimpleMesh As String = "Homeworld2 Simple Mesh File"
-    Public Const Name_WireframeMesh As String = "Homeworld2 Wireframe Mesh File"
-End Class
